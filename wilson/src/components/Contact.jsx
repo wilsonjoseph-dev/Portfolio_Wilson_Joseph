d957ab59198787b4f38e911e575033523657a067
wilsonjoseph-dev <wilsoneee21@gmail.com>
2024-03-24 20:52:30 +0530

    updated all files

--- wilson/src/components/Contact.jsx
+++ wilson/src/components/Contact.jsx
@@ -4,20 +4,48 @@ import Instagram from "../assets/instagram.png";
 import Facebook from "../assets/facebook.png";
 import Linkedin from "../assets/linkedin.png";
 import Twitter from "../assets/twitter.png";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_b9343il",
+        "template_7s29w4z",
+        form.current,
+        "aSA3I4-TvePYqJSDF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <article className="ContactSec" id="ContactSec">
       <h3 className="ContactHead">Contact me</h3>
       <p className="ContactDesc">
         Please fill out the form below to discuss any work opputunities
       </p>
-      <form action="" className="ContactForm">
-        <input type="text" placeholder="Enter Your Name" className="Name" />
+      <form action="" className="ContactForm" ref={form} onSubmit={sendEmail}>
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          className="Name"
+          name="from_name"
+        />
         <input
           type="email"
           placeholder="Enter Your Email Id"
           className="Email"
+          name="from_email"
         />
         <textarea
           placeholder="Enter Your Message Here"
